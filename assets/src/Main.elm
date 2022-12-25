916532a3b0872d3b023a464d8fb9bd5f1eaecb9f
Rob <rob.nice@protonmail.com>
2022-12-24 20:34:56 -0500

    add hover animation for multichoice tile, workout user defined fill in blank sentence concept

--- assets/src/Main.elm
+++ assets/src/Main.elm
@@ -47,15 +47,55 @@ main =
 
 
 squareTileButton label_ class_ =
+    let
+        classes =
+            [ "bg-blue-500 hover:bg-blue-600"
+            , "text-white font-bold text-4xl"
+            , "border-b-4 border-blue-700 hover:border-blue-500 rounded "
+            , "py-2 px-4"
+            , "transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110"
+            , class_
+            ]
+                |> String.join " "
+    in
     button
-        [ class <| "bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded " ++ class_
-        ]
+        [ class classes ]
         [ text label_ ]
+
+
+
+{-
+   Quiz Type
+   Fill in the blank with image
+
+   User needs to select or link an image
+
+   Have user construct sentence using 2 different tiles:
+     -Text tile, they select this and enter static text
+     - Multi Choice tile, they select this and add choices
+
+   Sentence and blanks are constructed in the order the users clicks and fills in the tiles
+
+   ex: Text Tile -> enters text, Multi Choice Tile -> Adds two optons, Multi Choice Tile -> enters text and two options
+
+           Text Tile   Multi Choice    Multi Choice with text
+   output: 彼女はドア　    「を、が」           開 [ける, かる]
+
+
+   type SentenceTile =
+       TextTile String
+       MultiChoiceTile String List(String)
+
+   type alias FillInTheBlank = {
+     image : String
+     , sentence : List(SentenceTile)
+   }
+-}
 
 
 popupChoice =
     div [ class "relative border-4 border-blue-400 h-12 w-12" ]
-        [ div [ class "absolute top-0 flex", style "top" "-55px", style "left" "-25px" ]
+        [ div [ class "absolute top-0 flex", style "top" "-67px", style "left" "-51px" ]
             [ squareTileButton "を" "mr-2"
             , squareTileButton "が" ""
             ]
@@ -65,10 +105,10 @@ popupChoice =
 quiz =
     div [ class "flex flex-wrap justify-center" ]
         [ img [ class "", src "https://www.saysinter.com/reform/column/wp-content/uploads/2016/02/No019_img02.png" ] []
-        , div [ class "w-full flex justify-center mt-20" ]
-            [ span [] [ text "ドア" ]
+        , div [ class "w-full flex justify-center mt-20 text-2xl items-center" ]
+            [ text "ドア"
             , popupChoice
-            , span [] [ text "" ]
+            , text ""
             ]
         ]
 
--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.elm", "../lib/ja_study_tools_web/templates/**/*.html.heex"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'zoom-once': 'zoom .8s ease-in-out',
+      },
+      keyframes: {
+        zoom: {
+          '0%, 100%': { transform: 'scale(1, 1)' },
+          '50%': { transform: 'scale(1.3, 1.3)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
